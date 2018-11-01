3fcca2dfbf9a2740e457cb890e0b639539188fb7
toonflash <toonflash@hotmail.com>
2018-11-01 04:10:06 +0000

    Wednesday night clean up before bed

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React, {Component} from 'react';
 import Chatbar from './Chatbar.jsx';
 import MessageList from './MessageList.jsx';
 
-// import data from './posts.json';
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -17,11 +15,10 @@ class App extends Component {
   }
   addNewUsername(event) {
     // console.log('newusername', event.target.value);
-    this.setState({ currentUser: {name: event.target.value}});
+    this.setState({ currentUser: { name: event.target.value }});
   }
   addNewMessage(newMessage) {
     // console.log('newMessage', newMessage);
-
     const msg = {
       type: "message",
       text: newMessage,
@@ -33,19 +30,15 @@ class App extends Component {
   componentDidMount() {
     this.socket.onmessage = (event) => {
       const incommingMessage = JSON.parse(event.data);
-
       //console.log('new msg from server!!!!!:', incommingMessage);
-
       const newMessageFromServer = {
         id: incommingMessage.id,
         username: incommingMessage.username,
         content: incommingMessage.content
       };
-
       const messages = this.state.messages.concat(newMessageFromServer);
       this.setState({ messages: messages });
     }
-
   }
   render() {
     //console.log(this.state.currentUser);
@@ -55,11 +48,13 @@ class App extends Component {
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
         <MessageList messages={ this.state.messages } />
-        <Chatbar userName={ this.state.currentUser.name } onCompleteMessage={ this.addNewMessage } updateName={ this.addNewUsername } />
+        <Chatbar
+          userName={ this.state.currentUser.name }
+          onCompleteMessage={ this.addNewMessage }
+          updateName={ this.addNewUsername } />
       </div>
     );
   }
 }
-
 
 export default App;
--- src/Message.jsx
+++ src/Message.jsx
@@ -2,19 +2,13 @@ import React, {Component} from 'react';
 
 function Message(props) {
     return (
-    <div>
-        <div className="message">
-            <span className="message-username">{props.user}</span>
-            <span className="message-content">{props.content}</span>
+        <div>
+            <div className="message">
+                <span className="message-username">{props.user}</span>
+                <span className="message-content">{props.content}</span>
+            </div>
         </div>
-    </div>
     )
 }
 
 export default Message;
-
-{/*
-<div className="message system">
-            Anonymous1 changed their name to nomnom.
-        </div>
-     */}
